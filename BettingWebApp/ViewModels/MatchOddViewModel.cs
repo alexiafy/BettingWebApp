3e6967712860f9aab0e1ffc3549299d299d3b040
alexi <alexia.fytili@gmail.com>
2022-09-26 10:13:19 +0300

    Minor change

--- BettingWebApp/ViewModels/MatchOddViewModel.cs
+++ BettingWebApp/ViewModels/MatchOddViewModel.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 
 namespace BettingWebApp.ViewModels
 {
+    //This class eventually was not used, but it might be used
+    //for updates in the near future, thus, I will not delete it
     public class MatchOddViewModel
     {
         public Match Match { get; set; }
